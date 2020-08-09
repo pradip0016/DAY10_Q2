19c3fa7c8c89c413158ca2480563fcd8ec5bddb7
pradip0016 <patilpradip980@gmail.com>
2020-08-09 13:27:14 +0530

    uc3

--- Flipcoin.sh
+++ Flipcoin.sh
@@ -4,7 +4,7 @@
 echo "code for flip coin simulation"
 Head=1
 Tail=1
-for i in {1..10}
+for i in {1..43}
 do
 	check=$(( RANDOM%2 ))
 	if [[ $check -eq 1 ]]
@@ -13,6 +13,19 @@ do
 	else
 		((Tail++))
 	fi
+
+	if [[ $Head -eq 21 || $Tail -eq 21 ]]
+	then
+		break
+	fi
 done
+
 echo "Total Head wons "$Head "time"
 echo "Total Tail wons "$Tail "time"
+
+if [[ $Head -gt $Tail ]]
+then
+	echo "Heads have won by "$(( $Head - $Tail ))
+else
+	echo "Tails have won by "$(( $Tail - $Head ))
+fi
