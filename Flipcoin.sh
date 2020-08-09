01f43f699111b87ee45aaca8b7cf3e0c3e59f0a2
pradip0016 <patilpradip980@gmail.com>
2020-08-09 12:59:29 +0530

    uc1

--- Flipcoin.sh
+++ Flipcoin.sh
@@ -2,3 +2,11 @@
 # code for flip coin simulation problem
 
 echo "code for flip coin simulation"
+
+check=$(( RANDOM%2 ))
+if [[ $check -eq 1 ]]
+then
+	echo "Heads Winner"
+else
+	echo "Tails Winner"
+fi
